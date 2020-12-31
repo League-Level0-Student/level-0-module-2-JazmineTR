e39c26ec429841fef556f620f8b4e05ee2ad522e
root <root@c7bd8a0a4aa6>
2020-12-31 23:27:29 +0000

    Finish Validation

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -17,7 +17,25 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if(randomNumber == 0) {
+			JOptionPane.showMessageDialog(null," Your smile is contagious");
+		}
+		if(randomNumber == 1) {
+			JOptionPane.showMessageDialog(null,"You have a great sense of humor");
+			
+		}
+		if(randomNumber == 2) {
+			JOptionPane.showMessageDialog(null,"You're like sunshine on a rainy day");
+		}
+		if(randomNumber == 3) {
+			JOptionPane.showMessageDialog(null," You bring out the best in other people");
+		}
+		if(randomNumber == 4) {
+			JOptionPane.showMessageDialog(null," Our community is better because you're in it");
+		}
+		if(randomNumber == 5) {
+			JOptionPane.showMessageDialog(null," You're a gift to those around you");
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
